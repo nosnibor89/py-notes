8c2b7cfe69b3d3c29e6349b43bb60f141771a280
Robinson Marquez <nosnibor1989@gmail.com>
2020-03-25 19:30:40 -0300

    Add models configuration and migrations

--- __init__.py
+++ __init__.py
@@ -1,13 +1,14 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY', default='dev'),
-        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI', default='sqlite:////tmp/test.db')
+        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI', default='sqlite:///tmp/test.db')
         # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
@@ -15,5 +16,9 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
+
+    from .models import db
+    Migrate(app, db)
+    db.init_app(app)
 
     return app
---
+++ models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Base(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_on = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255))
+    notes = db.relationship('Note', backref=db.backref('author', lazy=True))
+
+
+class Note(Base):
+    __tablename__ = 'notes'
+
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    # user = db.relationship('User', backref=db.backref('notes', lazy=True))
