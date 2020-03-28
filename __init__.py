793123a89b57e09ed45206d07577b40558d6ebbe
Robinson Marquez <nosnibor1989@gmail.com>
2020-03-28 20:07:57 -0300

    Adding authentication

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 import os
 
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flask_migrate import Migrate
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def create_app(test_config=None):
@@ -48,8 +48,44 @@ def create_app(test_config=None):
 
         return render_template('sign_up.html')
 
-    @app.route('/login')
+    @app.route('/login', methods=('GET', 'POST'))
     def log_in():
+
+        if request.method == 'POST':
+            error = None
+            username = request.form['username']
+            password = request.form['password']
+            if not username:
+                error = 'Username is required'
+            elif not password:
+                error = 'Password is required'
+            else:
+                user = User.query.filter_by(username=username).first()
+
+                if not user or not check_password_hash(user.password, password):
+                    error = 'Incorrect username or password'
+
+            if not error:
+                flash('Welcome to the notes app', 'success')
+                session.clear()
+                session['user_id'] = user.id
+                return redirect(url_for('notes'))
+
+            flash(error, 'error')
+
         return render_template('log_in.html')
+
+    @app.route('/logout')
+    def log_out():
+        session.clear()
+        flash('Successfully logged out.', 'success')
+        return redirect(url_for('log_in'))
+
+    @app.route('/')
+    def notes():
+        if not session.get('user_id'):
+            return redirect(url_for('log_in'))
+
+        return 'Notes view'
 
     return app
